ce971c3dcea41010bf59ca3a2802d048b4f6ef55
Sterling Walsh <sterling.walsh.dev@gmail.com>
2019-02-18 00:04:19 -0500

    making all basic functionality work

--- scripts/card.js
+++ scripts/card.js
@@ -1,17 +1,17 @@
 const cardDiv =`<div class='card'>
-                    <div class='card-front'>Front</div>
-                    <div class='card-back'>Back</div>
+                    <div class='card-front card-side match-parent flex-cntr ignore-click'><p>?</p></div>
+                    <div class='card-back card-side match-parent ignore-click'></div>
                 </div>`;
 
-const createCard = (id) => {
-    let element = stringToElement(cardDiv);
-    element.setAttribute('key', id);
-    return element;
+const createCard = (key, value) => {
+    const card = stringToElement(cardDiv);
+    card.setAttribute('key', key);
+    card.getElementsByClassName('card-back')[0].style.backgroundImage = `url('https://robohash.org/${value}')`;
+    return card;
 }
 
 const stringToElement = (html) => {
     const template = document.createElement('template')
-
     template.innerHTML = cardDiv;
     return template.content.childNodes[0];
 }
